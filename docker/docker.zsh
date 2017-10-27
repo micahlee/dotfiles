eb6de02fa9d41ba2f1794450aa129f69b13b9449
Jason Vanderhoof <jvanderhoof@gmail.com>
2017-10-27 08:58:05 -0600

    added shortcut for dangling image cleanup

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -9,3 +9,9 @@ function docker_exec() {
   docker exec -it "$@" /bin/bash
 }
 alias de="docker_exec"
+
+alias docker-cleanup="docker_cleanup"
+
+function docker_cleanup {
+  docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
+}
